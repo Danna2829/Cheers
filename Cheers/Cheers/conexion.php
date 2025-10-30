a14af42855d23e8e5ed4c6fba45d52173bc6b69b
Danna2829 <136909136+Danna2829@users.noreply.github.com>
2025-10-29 22:12:46 -0500

    Update conexion.php

--- Cheers/Cheers/conexion.php
+++ Cheers/Cheers/conexion.php
@@ -1,9 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; 
+$password = "Licoreria"; 
 $dbname = "Cheers";
-
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) die("Conexión fallida: " . mysqli_connect_error());
-?>
